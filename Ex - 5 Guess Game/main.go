e5a0b5e9ccbd291dc63f08c69e5a33cbc8976229
Yash <57724250+mezeru@users.noreply.github.com>
2021-02-06 01:20:05 +0530

    Add files via upload

---
+++ Ex - 5 Guess Game/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func rNum(max int) int {
+
+	return rand.Intn(max)
+
+}
+
+func main() {
+
+	fmt.Println("This is a Guessing game")
+	var in int
+	count := 0
+	tries := 10
+
+	num := rNum(100)
+
+	userinp := make(chan int)
+
+	fmt.Print("\nTake a Guess (You only get ", tries, " tries and max number is 100): ")
+
+	for count < tries {
+
+		go func() {
+			fmt.Scanf("%d\n", &in)
+			userinp <- in
+		}()
+
+		count++
+
+		select {
+
+		case user := <-userinp:
+			{
+				if user > num {
+					fmt.Print("Guess Lower : ")
+				} else if user < num {
+					fmt.Print("Guess Higher : ")
+				} else {
+					fmt.Println("\nCorrect !!!!")
+					fmt.Println("You Got the number right in ", count+1, "tries")
+					return
+				}
+			}
+		}
+
+	}
+
+	if count == tries {
+		fmt.Println("\nYou Have Lost this Round \n The number was ", num)
+	}
+
+}
