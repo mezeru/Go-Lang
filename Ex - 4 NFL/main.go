7bcf53ab4028bad90a0d91116546e60dd8bb8c29
Yash <57724250+mezeru@users.noreply.github.com>
2021-02-05 17:18:44 +0530

    Add files via upload

---
+++ Ex - 4 NFL/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+	"time"
+
+	"nhlApi"
+)
+
+func main() {
+	now := time.Now()
+
+	rosterFile, err := os.OpenFile("rosters.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening the file rosters.txt: %v", err)
+	}
+	defer rosterFile.Close()
+
+	wrt := io.MultiWriter(os.Stdout, rosterFile)
+
+	log.SetOutput(wrt)
+
+	teams, err := nhlApi.GetAllTeams()
+	if err != nil {
+		log.Fatalf("error while getting all teams: %v", err)
+	}
+
+	for _, team := range teams {
+		log.Println("---------------------")
+		log.Printf("Name: %s", team.Name)
+		log.Println("---------------------")
+	}
+
+	log.Printf("took %v", time.Now().Sub(now).String())
+}
